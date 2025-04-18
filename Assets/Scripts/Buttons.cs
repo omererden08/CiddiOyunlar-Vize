a5be7acdb83293813348bbc394dd0e23a514670d
OzkanCetin <ozkancetin@posta.mu.edu.tr>
2025-04-18 15:12:55 +0300

    test push

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -2,24 +2,44 @@ using UnityEngine;
 
 public class Buttons : MonoBehaviour
 {
+    private AudioSource audioSource;
+
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if(audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+    }
+
     public void StartGame()
     {
+        audioSource.Play();
         FadeManager.Instance.FadeToScene("Gameplay");
     }
     public void ReturnMenu()
     {
+        audioSource.Play();
         GameManager.Instance.isGameOver = false;
         GameManager.Instance.ResetCounters();
         FadeManager.Instance.FadeToScene("MainMenu");
     } 
     public void Restart()
     {
+        audioSource.Play();
         GameManager.Instance.isGameOver = false;
         GameManager.Instance.ResetCounters();
         FadeManager.Instance.FadeToScene("Gameplay");
     } 
     public void Quit()
     {
+        audioSource.Play();
         FadeManager.Instance.Quit();
     }    
 }
