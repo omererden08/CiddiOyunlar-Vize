a33f03975fa987c77fb6e705d51dfcc7d2a6bd35
Ömer Erden <omererden08@outlook.com>
2025-04-18 16:19:05 +0300

    last updates

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,45 +1,76 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Buttons : MonoBehaviour
 {
     private AudioSource audioSource;
-
+    private Image howtoPlay;
+    private bool isOpened = false;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        howtoPlay = GameObject.Find("HowToPlay").GetComponent<Image>();
+        howtoPlay.gameObject.SetActive(false);
     }
 
     void Update()
     {
-        if(audioSource == null)
+        if (audioSource == null)
         {
             audioSource = GetComponent<AudioSource>();
+        }
+        if (howtoPlay == null)
+        {
+            howtoPlay = GameObject.Find("HowToPlay").GetComponent<Image>();
+            howtoPlay.gameObject.SetActive(false);
         }
     }
 
     public void StartGame()
     {
         audioSource.Play();
+        if (isOpened)
+        {
+            howtoPlay.gameObject.SetActive(false);
+            isOpened = false;
+        }
+
         FadeManager.Instance.FadeToScene("Gameplay");
     }
+
+    public void HowtoPlay()
+    {
+        audioSource.Play();
+        if (isOpened)
+        {
+            howtoPlay.gameObject.SetActive(false);
+            isOpened = false;
+        }
+        else
+        {
+            howtoPlay.gameObject.SetActive(true);
+            isOpened = true;
+        }
+    }
+
     public void ReturnMenu()
     {
         audioSource.Play();
         GameManager.Instance.isGameOver = false;
         GameManager.Instance.ResetCounters();
         FadeManager.Instance.FadeToScene("MainMenu");
-    } 
+    }
     public void Restart()
     {
         audioSource.Play();
         GameManager.Instance.isGameOver = false;
         GameManager.Instance.ResetCounters();
         FadeManager.Instance.FadeToScene("Gameplay");
-    } 
+    }
     public void Quit()
     {
         audioSource.Play();
         FadeManager.Instance.Quit();
-    }    
+    }
 }
